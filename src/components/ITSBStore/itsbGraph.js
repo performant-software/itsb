5f09d4acc4e58cb4a0f3310671cd7b7456059660
Rainer Simon <rainer@rainersimon.io>
2022-10-10 09:52:08 +0200

    Started ITSB graph implementation: authors, places (+test)

--- src/components/ITSBStore/itsbGraph.js
+++ src/components/ITSBStore/itsbGraph.js
@@ -1,5 +1,54 @@
 import createGraph from 'ngraph.graph';
 
+// Just makes sure there's an 'id' prop, and normalize 'type'
+const normalizeNode = n => {
+  if (n.id) 
+    return n;
+
+  n.id = n['@id'];
+  delete n['@id'];
+
+  if (!n.id)
+    throw { message: 'Missing node ID', node: n };
+
+  if (n['@type']) {
+    n.type = n['@type'];
+    delete n['@type'];
+  }
+
+  return n;
+}
+
+// Itinerary to waypoints
+const splitItinerary = it => {
+  const itineraryId = it.id;
+  const authorId = it.target.id;
+  const waypointList = it.body.value;
+
+  // Work around a conceptual difference between LP and our
+  // graph model: 'id' in LP refers to the place; but it 
+  // identifies the waypoint node itself in our graph!
+  return waypointList.map((value, idx) => ({
+    ...value,
+    place: value.id,
+    id: `${itineraryId}/wp/${idx}`
+  }));
+}
+
+/**
+ * A domain graph model for ITSB with the following 
+ * types of nodes:
+ * 
+ * - author (id, name)
+ * - place (GeoJSON/LP feature)
+ * - waypoint (one stop on the itinerary, linked to 
+ *   author, place, citations, and scoped by time
+ * 
+ * Edges:
+ * - waypoints are linked to author
+ * - waypoints are linked to places
+ * - waypoints are (directionally!) linked to waypoints
+ */
 export class ITSBGraph {
 
   constructor() {
@@ -7,19 +56,49 @@ export class ITSBGraph {
   }
 
   init = (authors, places, itineraries) => {
-    // TODO we'll build a graph model
-    // with the following types of nodes:
-    // - Author (id, name)
-    // - Waypoint (one stop on the itinerary, 
-    //   linked to the author, the place, citations, 'when')
-    // - Place (GeoJSON/LP feature)
+    this.graph.clear();
 
-    // Edges:
-    // - Authors linked to waypoints
-    // - Waypoints linked to places
-    // - Waypoints linked to waypoints (directional?)
+    // Authors and places can be treated the same way
+    const authorNodes = authors.map(normalizeNode);
+    const placeNodes = places.map(normalizeNode);
+
+    [...authorNodes, ...placeNodes ].forEach(n => 
+      this.graph.addNode(n.id, n));
+
+    // Itineraries are split to waypoint nodes
+    const waypointNodes = itineraries.reduce((waypoints, it) => {
+      return [...waypoints, ...splitItinerary(it)]
+    }, []);
+
+    console.log(waypointNodes);
+
+    waypointNodes.forEach(n => this.graph.addNode(n.id, n));
   }
 
+  listNodesWithType = type => {
+    const nodes = [];
+
+    this.graph.forEachNode(({ data }) => {
+      if (data.type === type)
+        nodes.push(data);
+    });
+
+    return nodes;
+  }
+
+  listAuthors = () =>
+    this.listNodesWithType('Person');
+
+  listPlaces = () =>
+    this.listNodesWithType('Feature');
+
+  listItineraries = () => {
+
+  }
+
+  listWaypoints = () => {
+
+  }
 
   /**
    * Queries - trying to keep as generic and close
--- src/components/ITSBStore/itsbGraph.test.js
+++ src/components/ITSBStore/itsbGraph.test.js
@@ -1,9 +1,34 @@
 import { describe, it, expect } from 'vitest';
+import { ITSBGraph } from './itsbGraph';
+
+import authors from '../../../data/authors.json';
+import places from '../../../data/places.json';
+import itineraries from '../../../data/itineraries.json';
+
+const initGraph = (authors, places, itineraries) => {
+  const graph = new ITSBGraph()
+  graph.init(authors, places, itineraries)
+  return graph;
+}
 
 describe('ITSBGraph', () => {
 
-  it('should run the test', () => {
-    expect(1).toBe(1);
-  })
+  it('should load authors correctly', () => {
+    const graph = initGraph(authors.itemListElement, [], [])
+    const expected = graph.listAuthors();
+    expect(expected.length).toBe(12);
+  });
+
+  it('should load places correctly', () => {
+    const graph = initGraph([], places.features, []);
+    const expected = graph.listPlaces();
+    expect(expected.length).toBe(488);
+  });
+
+  it('should load itineraries correctly', () => {
+    const graph = initGraph([], [], itineraries.first.items);
+    const expected = graph.listItineraries();
+    expect(expected.length).toBe(12);
+  });
 
 });

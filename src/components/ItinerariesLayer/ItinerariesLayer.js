19aaaf523a9f8df237843775c8c497dc1b065207
Ben Silverman <ben@performantsoftware.com>
2022-10-31 10:33:55 -0400

    Add GeoJSON layer of unique places (#5)

--- src/components/ItinerariesLayer/ItinerariesLayer.js
+++ src/components/ItinerariesLayer/ItinerariesLayer.js
@@ -1,4 +1,4 @@
-import { ArcLayer } from '@peripleo/peripleo/deck.gl';
+import { ArcLayer, GeoJsonLayer } from '@peripleo/peripleo/deck.gl';
 
 const toArc = (waypoints, graph) => {
   const trajectory = [];
@@ -14,28 +14,68 @@ const toArc = (waypoints, graph) => {
 };
 
 /**
- * The task of the ItineraryLayer is to take a search result,
- * and turn the itinerary trajectories into DeckGL layers.
+ * Convenience function to get unique places across the list of itineraries.
+ *
+ * @param {Array<object>} resultItems A list of itinerary objects (author, waypoints)
+ * @param {ITSBGraph} graph The graph used to lookup place geometries
+ * @returns {Array<object>} Array of unique GeoJSON Features
  */
-export const ItinerariesLayer = (resultItems, graph) => {
-  return resultItems.map(({ author, waypoints }) => {
-    const arc = toArc(waypoints, graph);
+function getUniquePlaces(resultItems, graph) {
+  const places = [];
 
-    const { color } = graph.getNode(author);
-
-    return new ArcLayer({
-      id: `${author}/trajectory`,
-      data: arc,
-      pickable: false,
-      getWidth: 1.5,
-      widthMinPixels: 1,
-      getHeight: 1,
-      getTilt: 10,
-      greatCircle: false,
-      getSourcePosition: (d) => d.from.coordinates,
-      getTargetPosition: (d) => d.to.coordinates,
-      getSourceColor: color,
-      getTargetColor: color,
+  resultItems.forEach(({ waypoints }) => {
+    waypoints.forEach((waypoint) => {
+      if (!places.some((place) => place.id === waypoint.place)) {
+        places.push(graph.getNode(waypoint.place));
+      }
     });
   });
+
+  return places;
+}
+
+/**
+ * The task of the ItineraryLayer is to take a search result,
+ * and turn the itinerary trajectories into DeckGL layers.
+ *
+ * @returns {Array<Layer>} Array of DeckGL layers to map
+ */
+export const ItinerariesLayer = (resultItems, graph) => {
+  return [
+    new GeoJsonLayer({
+      id: 'trajectories-places',
+      data: getUniquePlaces(resultItems, graph),
+      pickable: true,
+      filled: true,
+      stroked: true,
+      lineWidthScale: 10,
+      lineWidthMinPixels: 2,
+      getLineColor: [252, 176, 64, 200],
+      getFillColor: [252, 176, 64, 100],
+      getPointRadius: 10000,
+      pointRadiusMinPixels: 6,
+      getLineWidth: 10,
+      pointType: 'circle',
+    }),
+    ...resultItems.map(({ author, waypoints }) => {
+      const arc = toArc(waypoints, graph);
+
+      const { color } = graph.getNode(author);
+
+      return new ArcLayer({
+        id: `${author}/trajectory`,
+        data: arc,
+        pickable: false,
+        getWidth: 1.5,
+        widthMinPixels: 1,
+        getHeight: 1,
+        getTilt: 10,
+        greatCircle: false,
+        getSourcePosition: (d) => d.from.coordinates,
+        getTargetPosition: (d) => d.to.coordinates,
+        getSourceColor: color,
+        getTargetColor: color,
+      });
+    }),
+  ];
 };

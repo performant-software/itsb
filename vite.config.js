1272a2d3624925792d476f8e6390a18e8a66dcfb
Ben Silverman <ben@performantsoftware.com>
2022-10-31 10:33:36 -0400

    Serve dev on "localhost" instead of resolving IP

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
+import dns from 'dns';
+
+// serve dev on "localhost" instead of resolving to 127.0.0.1
+dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],

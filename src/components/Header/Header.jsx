633dc3ba3dbd8431430388675db5cfff797a13ca
Ben Silverman <ben@performantsoftware.com>
2022-11-22 16:37:46 -0500

    Mobile navigation (#48)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
 import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
+/**
+ * Header component, containing navigation links to pages in the application.
+ *
+ * @returns {React.Component} Header React functional component
+ */
 export function Header() {
   return (
     <header>
       <nav>
+        <div className="home">
+          <Link to="/">
+            <span>In The Same Boats</span>
+          </Link>
+        </div>
+        <input id="mobile-nav-toggle" name="mobile-nav-toggle" type="checkbox" />
+        <label htmlFor="mobile-nav-toggle">
+          <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+          </svg>
+        </label>
         <ul>
-          <li>
-            <Link to="/" className="home">
-              <span>In The Same Boats</span>
-            </Link>
-          </li>
           <li>
             <NavLink to="instructions">Instructions</NavLink>
           </li>

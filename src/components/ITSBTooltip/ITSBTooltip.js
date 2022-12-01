4c27b513375614e0e8c9f08cc57dd2e0fcb127c8
Ben Silverman <ben@performantsoftware.com>
2022-12-01 16:17:13 -0500

    Add detail to tooltips in trajectories viz (#35)

--- src/components/ITSBTooltip/ITSBTooltip.js
+++ src/components/ITSBTooltip/ITSBTooltip.js
@@ -1,3 +1,68 @@
-export const ITSBTooltip = ({ object }) => {
-  return object?.properties?.title;
-};
+import { sortWaypointsByTime } from '../ITSBStore';
+
+/**
+ * Format the date by moving the year to the end and changing dash delimiters
+ * to slashes; or return a question mark if there is no date.
+ *
+ * @param {string} date The date as a string formatted YYYY-MM-DD.
+ * @returns {string} Date formatted MM/DD/YYYY.
+ */
+function fmt(date) {
+  const split = date?.split('-');
+  if (split?.length) {
+    split.push(split.shift());
+  }
+  return split?.join('/') || '?';
+}
+
+/**
+ * Generate a tooltip string for a waypoint in an itinerary, or a place with
+ * intersecitons.
+ *
+ * @param {*} props Arguments object to destructure
+ * @param {*} props.graph The ITSB graph context provider
+ * @param {*} props.object The currently selected waypoint
+ * @param {*} props.search The ITSB search context provider
+ * @returns {string|undefined} The tooltip text, if possible
+ */
+export function ITSBTooltip({ graph, object, search }) {
+  if (object) {
+    if (object.present) {
+      // present means this tooltip is for intersections; just use place name
+      return object.properties.title;
+    } else {
+      // for trajectories, show place name and labeled waypoints
+      const selected = [object.properties.title];
+
+      // Get all waypoints on this place
+      const waypointsAtThisPlace = search.result.items?.reduce((all, it) => {
+        const { waypoints } = it;
+        return [...all, ...waypoints.filter((wp) => wp.place === object.id)];
+      }, []);
+      // sort them by time
+      const sorted = waypointsAtThisPlace && sortWaypointsByTime(waypointsAtThisPlace);
+      const anyHasLabel = sorted.some((waypoint) => waypoint.relation.label);
+      sorted.forEach((waypoint) => {
+        const { author, relation, when } = waypoint;
+        // if this waypoint has relation.label or NONE on this place do, then
+        // add it to the tooltip.
+        // if some waypoint on this place has relation.label but this one does
+        // not, skip this waypoint.
+        if (relation.label || !anyHasLabel) {
+          const { name } = graph.getNode(author);
+          // format is "Name: Label (StartDate–EndDate)"
+          let label = relation.label ? `${name}: ${relation.label}` : name;
+          if (when.timespans.length) {
+            const { start, end } = when.timespans[0];
+            const startDate = start?.in || start?.earliest;
+            const endDate = end?.in || end?.latest;
+            label = `${label} (${fmt(startDate)}–${fmt(endDate)})`;
+          }
+          selected.push(label);
+        }
+      });
+      // since we can only use strings here, join by newline (will become <br>)
+      return selected.join('\n');
+    }
+  }
+}
